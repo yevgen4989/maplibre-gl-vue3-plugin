cd78393bdc17c5d6e5580c92989fa817958106cf
yevgen4989 <yevgenpisotsky@outlook.com>
2022-10-07 14:29:27 +0300

    add export new components

--- src/components/index.ts
+++ src/components/index.ts
@@ -8,7 +8,9 @@ export { default as MglGeolocationControl } from './controls/geolocation.control
 export { default as MglNavigationControl } from './controls/navigation.control';
 export { default as MglScaleControl } from './controls/scale.control';
 export { default as MglStyleSwitchControl } from './controls/styleSwitch.control';
+export { default as MglLangSwitchControl } from  './controls/langSwitch.control';
 export { default as MglButton } from './button.component';
+export { default as MdiIcon } from './icon.component';
 export { default as MglMarker } from './marker.component';
 export { default as MglCanvasSource } from './sources/canvas.source';
 export { default as MglGeoJsonSource } from './sources/geojson.source';
